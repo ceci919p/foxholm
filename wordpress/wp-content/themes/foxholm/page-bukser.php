283577afeb9e7b4e6a363d02a71cf834abfc3265
Caroline Nielsen <caro112f@stud.kea.dk>
2021-12-10 16:12:04 +0100

    test på sti

--- wordpress/wp-content/themes/foxholm/page-bukser.php
+++ wordpress/wp-content/themes/foxholm/page-bukser.php
@@ -10,6 +10,52 @@ $container_class = apply_filters( 'neve_container_class_filter', 'container', 's
 get_header();
 
 ?>
+	<div class="nv-single-page-wrap col">
+			<?php
+			/**
+			 * Executes actions before the page header.
+			 *
+			 * @since 2.4.0
+			 */
+			do_action( 'neve_before_page_header' );
+
+			/**
+			 * Executes the rendering function for the page header.
+			 *
+			 * @param string $context The displaying location context.
+			 *
+			 * @since 1.0.7
+			 */
+			do_action( 'neve_page_header', 'single-page' );
+
+			/**
+			 * Executes actions before the page content.
+			 *
+			 * @param string $context The displaying location context.
+			 *
+			 * @since 1.0.7
+			 */
+			do_action( 'neve_before_content', 'single-page' );
+
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'template-parts/content', 'page' );
+				}
+			} else {
+				get_template_part( 'template-parts/content', 'none' );
+			}
+
+			/**
+			 * Executes actions after the page content.
+			 *
+			 * @param string $context The displaying location context.
+			 *
+			 * @since 1.0.7
+			 */
+			do_action( 'neve_after_content', 'single-page' );
+			?>
+		</div>
 <template>
             <article>
                 
