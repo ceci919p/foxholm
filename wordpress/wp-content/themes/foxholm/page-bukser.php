6c2f18acabe9e8e77c0321d734ae17162255b256
Caroline Nielsen <caro112f@stud.kea.dk>
2021-12-10 15:22:24 +0100

    ændret til neve templates

--- wordpress/wp-content/themes/foxholm/page-bukser.php
+++ wordpress/wp-content/themes/foxholm/page-bukser.php
@@ -1,25 +1,14 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all pages.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * @package Neve
+ * @since   1.0.0
  */
+$container_class = apply_filters( 'neve_container_class_filter', 'container', 'single-page' );
 
 get_header();
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
 ?>
 <template>
 			<article>
@@ -134,8 +123,4 @@ endwhile; // End of the loop.
 		</script>
 		
 	</div><!-- #primary -->
-
-<?php
-
-
-get_footer();
+<?php get_footer(); ?>
