913b8bc549de7a8396ffb5c7c14614fe4e7e0152
ceci919p <ceci919p@stud.kea.dk>
2021-12-14 17:14:57 +0100

    singleview css and html structure

--- wordpress/wp-content/themes/foxholm/single-buks.php
+++ wordpress/wp-content/themes/foxholm/single-buks.php
@@ -14,21 +14,38 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+		<div class="nv-page-title ">
+		<small class="nv--yoast-breadcrumb neve-breadcrumbs-wrapper">
+			<span>
+				<span>
+					<a href="https://ceciliejasmin.dk/kea/10_eksamensprojekt/foxholm/wordpress/">Home</a> » 
+					<a href="https://ceciliejasmin.dk/kea/10_eksamensprojekt/foxholm/wordpress/bukser/">Bukser</a> »
+					<span class="breadcrumb_last" aria-current="page">Detaljer</span>
+				</span>
+			</span>
+		</small>
+		
+		
+					</div>
+
 			<article>
 				<button class="luk">Tilbage</button>
 				<section class="single_container">
 				<img class="pic" src="" alt="">
 
 				<div class="single_text">
-					<h2></h2>
+					<div class="text_wrapper">
+						<h2></h2>
 						<p class="pris"></p>
 						<p class="maerke"></p>
 						<p class="stoerrelse"></p>
 						<p class="materiale"></p>
-					
-						<button>køb</button>
-			
-			    </div>
+						
+						<div class="button_wrapper">
+						<button>KØB</button>
+					    </div>
+					</div>
+				</div>
 			    </section>
 			</article>
 
@@ -49,10 +66,10 @@ get_header();
 		function visBuks () {
 			document.querySelector("h2").textContent = buks.title.rendered;
 			document.querySelector(".pic").src = buks.billede.guid;
-			document.querySelector(".pris").innerHTML = buks.pris + " kr";
-			document.querySelector(".maerke").textContent = buks.maerke;
-			document.querySelector(".stoerrelse").textContent = buks.stoerrelse;
-			document.querySelector(".materiale").textContent = buks.materiale;
+			document.querySelector(".pris").innerHTML = "Pris:  " + buks.pris + " kr";
+			document.querySelector(".maerke").innerHTML = "Mærke:  " + buks.maerke;
+			document.querySelector(".stoerrelse").innerHTML = "Størrelse:  " + buks.stoerrelse;
+			document.querySelector(".materiale").innerHTML = "Materiale:  " + buks.materiale;
 		}
 
 		document.querySelector(".luk").addEventListener("click", () => {
